9bc3c2cbb4111f10e75c7cbee2cd57dbf69eb552
Hari Kiran <113755124+harikiran5184@users.noreply.github.com>
2023-11-13 15:53:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,112 +1,36 @@
-import requests
-from flask import Flask, jsonify, request
-from flask_cors import CORS
+from flask import Flask, jsonify
 from bs4 import BeautifulSoup
+import requests
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "*"}})
 
-def scrape_link(url: str) -> str:
-    req = requests.get(url).content
-    soup = BeautifulSoup(req, "html.parser")
-    link = soup.find("a", {"class": "main-button dlbutton"})['href']
-    return link
-
-def get_page(url: str) -> list:
-    req = requests.get(url).content
-    soup = BeautifulSoup(req, "html.parser")
-    divs = soup.find_all("div", class_="cont_display")
-    data = []
-    for i in range(2, len(divs)):
-        title = divs[i].find("a")
-        img = divs[i].find("img")
-        dat = {"title": title['title'], "image": img['src'], "link": title['href']}
-        data.append(dat)
-    return data
-
-def get_movie(url: str) -> dict:
-    req = requests.get(url).content
-    soup = BeautifulSoup(req, "html.parser")
-    title = soup.find("h2", class_="entry-title").text.replace("Full Movie Watch Online Free", "")
-    image = soup.find("img", class_="attachment-post-thumbnail size-post-thumbnail wp-post-image")['src']
-    description = soup.find_all("p")[4].text
-    torrents = soup.find_all("a", class_="mv_button_css")
-    torrent = []
-    other_links = []
-    for tor in torrents:
-        link = tor['href']
-        size = tor.find_all("small")[0].text
-        quality = tor.find_all("small")[1].text
-        data = {"magnet": link, "size": size, "quality": quality}
-        torrent.append(data)
-    ps = soup.find_all("p")
-    for p in ps:
-        if p.find("strong"):
-            if "Watch Online –" in p.find("strong").text:
-                typ = p.find("strong").text.split("–")[-1]
-                lin = p.find("a")['href']
-                try:
-                    lin = scrape_link(lin)
-                    data = {"type": typ, "url": lin}
-                    other_links.append(data)
-                except:
-                    data = {"type": typ, "url": lin}
-                    other_links.append(data)
-    data = {"status": True, "url": url, "title": title, "description": description, "image": image, "torrent": torrent, "other_links": other_links}
-    return data
-
-@app.route("/search", methods=["GET"])
-def search():
-    a = request.args.get("query")
-    url = f"https://ww7.5movierulz.gd/?s={a}"
+def scrape_data(url):
     try:
-        data = get_page(url)
-        total = len(data)
-        main_data = {"status": True, "total_found": total, "url": url, "data": data}
-    except:
-        main_data = {"status": False, "msg": "No Data Found"}
-    return jsonify(main_data)
+        response = requests.get(url)
+        soup = BeautifulSoup(response.content, 'html.parser')
 
-@app.route("/<language>/<int:page>")
-def get_home(language: str, page: int):
-    page = 1 if page is None else page
-    if language == "telugu":
-        url = f"https://ww7.5movierulz.gd/telugu-movie/page/{page}"
-    elif language == "hindi":
-        url = f"https://ww7.5movierulz.gd/bollywood-movie-free/page/{page}"
-    elif language == "tamil":
-        url = f"https://ww7.5movierulz.gd/tamil-movie-free/page/{page}"
-    elif language == "malayalam":
-        url = f"https://ww7.5movierulz.gd/malayalam-movie-online/page/{page}"
-    elif language == "english":
-        url = "https://ww7.5movierulz.gd/category/hollywood-movie-2023/"
-    else:
-        url = None
-    if url is not None:
-        data = get_page(url)
-        total = len(data)
-        main_data = {"status": True, "total_found": total, "url": url, "data": data}
-    else:
-        main_data = {"status": False}
-    return jsonify(main_data)
+        # Example: Extracting data from HTML elements
+        title = soup.title.string
+        paragraphs = [p.get_text() for p in soup.find_all('p')]
 
-@app.route("/")
-def home():
-    url = "https://ww7.5movierulz.gd/"
-    data = get_page(url)
-    total = len(data)
-    main_data = {"status": True, "total_found": total, "url": url, "data": data}
-    return jsonify(main_data)
+        # Example: Creating a data object
+        data = {
+            'title': title,
+            'paragraphs': paragraphs,
+        }
 
-@app.route("/get", methods=["GET"])
-def get_s():
-    a = request.args.get("url")
+        return data
+    except Exception as e:
+        raise e
+
+@app.route('/scrape')
+def scrape_route():
+    target_url = 'https://www.google.com/'
     try:
-        data = get_movie(a)
+        data = scrape_data(target_url)
         return jsonify(data)
-    except:
-        data = {"status": False, "msg": "Unable to get data"}
-        return jsonify(data)
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
